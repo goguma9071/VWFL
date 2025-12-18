44eb7d1ee9c1fc5a8eda65b2c470758e77c070ba
goguma <goguma9071@protonmail.com>
2025-12-18 20:27:21 +0900

    target folder deleted

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,5 @@
-// in src/cpu.rs
 
+use iced_x86::*;
 use crate::vm::Vm;
 
 const MAX_STEPS: usize = 20; // 우선 20번의 명령어만 실행하도록 제한
@@ -7,14 +7,15 @@ const MAX_STEPS: usize = 20; // 우선 20번의 명령어만 실행하도록 제
 /// VM을 받아 CPU 에뮬레이션 루프를 실행합니다.
 pub fn run(vm: &mut Vm) -> Result<(), &'static str> {
     println!("\n--- CPU Emulation Start ---");
+    let mut decoder = Decoder::new(64, &[], DecoderOptions::NONE);
 
     for step in 0..MAX_STEPS {
         // --- FETCH (명령어 가져오기) ---
-        // RIP가 가리키는 주소에서 1바이트를 읽어옵니다. 이것이 명령어 코드(Opcode)입니다.
+        // RIP가 가리키는 주소에서 1바이트를 읽어옴.
         let opcode = vm.read_byte(vm.rip)?;
 
         // --- DECODE & EXECUTE (해석 및 실행) ---
-        // 지금은 실제 명령을 해석하는 대신, 현재 상태를 출력만 합니다.
+        // 지금은 실제 명령을 해석하는 대신, 현재 상태를 출력만 함.
         println!(
             "[Step {:02}] RIP: 0x{:08x}, Fetched Opcode: 0x{:02x}",
             step, vm.rip, opcode
